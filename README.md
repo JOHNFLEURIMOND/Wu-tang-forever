57fbc927edb139525b5e4d930a6c7545c86dee64
JOHNFLEURIMOND <johnnyxxboston@gmail.com>
2018-03-12 00:17:36 -0400

    edits

---
+++ README.md
@@ -0,0 +1,18 @@
+![WU-TANG](img/wutang.png)
+
+# My Awesome Project
+This is an simple to-do list application that allows the user to be able to answer a survey question by populate the inputs and upon his/her answer they will receive the names of the Wu-Tang members.
+
+## How It's Made:
+HTML, CSS, JavaScript.
+
+
+## Optimizations
+I need to get the buttons to work and better style!
+
+## Lessons Learned:
+I learned how to use the ES6 code for JavaScript and made my writing my functions easier to write!
+
+## portfolio:
+
+**WEBSITE:** https:/johnfleurimond.com
--- main.js
+++ main.js
@@ -1,30 +1,32 @@
-//the user is going to play our wu-tang application
-//the user will be able to answer a survey question
+// the user is going to play our wu-tang application
+// the user will be able to answer a survey question
 // upon his answer he'll receive the names of the wutang members
 
-// const wuTang = ["rza","gza","u-god","odb","method man","raekwon","inspectah deck","masta killa","ghostface killah"];
-// const buttonPress = document.getElementById("press");
-//
-// let name = document.getElementById("firstName");
-// let yoHood = document.getElementById("hood");
-// let faveSong = document.getElementById("wuSong");
-// let yoAge = document.getElementById("age");
-// let howTall = document.getElementById("height");
-// let nameGenerated = document.getElementById("generated");
-//
-// let inputs = document.getElementsByTagName("input");
-// let bar =  inputs[i].value
-//
-// buttonPress.addEventListener("click", () => {
-//   for(let i = 0; i <= inputs.length; i++){
-//     if(bar !== ""){
-//       let random = wuTang[Math.round(Math.random()* wuTang.length - 1)];
-//       nameGenerated.innerHTML = random;
-//     }
-//   }
-// });
+const wuTang = ["rza", "gza", "u-god", "odb", "method man", "raekwon", "inspectah deck", "masta killa", "ghostface killah"];
+const buttonPress = document.getElementById("press");
+
+let name = document.getElementById("firstName");
+let yoHood = document.getElementById("hood");
+let faveSong = document.getElementById("wuSong");
+let yoAge = document.getElementById("age");
+let howTall = document.getElementById("height");
+let nameGenerated = document.getElementById("generated");
+
+let inputs = document.getElementsByTagName("input");
+let bar = inputs[i].value
+
+buttonPress.addEventListener("click", () => {
+  for (let i = 0; i <= inputs.length; i++) {
+    if (bar !== "") {
+      let random = wuTang[Math.round(Math.random() * wuTang.length - 1)];
+      nameGenerated.innerHTML = random;
+    }
+  }
+});
+
+
 alert("answer all questions, then submit to see which wu-tang character you are ")
-const wtArray = ["rza","gza","u-god","odb","method man","raekwon","inspectah deck","masta killa","ghostface killah"]
+const wtArray = ["rza", "gza", "u-god", "odb", "method man", "raekwon", "inspectah deck", "masta killa", "ghostface killah"]
 const push = document.getElementById("Wbutton")
 let name = document.getElementById("firstName");
 
@@ -39,10 +41,10 @@ let inputs = document.getElementsByTagName('input');
 let damn = inputs[1].value
 
 push.addEventListener("click", () => {
-  for(let i = 0; i <= inputs.length; i++){
+  for (let i = 0; i <= inputs.length; i++) {
     if (damn !== "") {
-    let random = wtArray[Math.round(Math.random()*wtArray.length - 1)];
-    nameBruh.innerHTML = random;
+      let random = wtArray[Math.round(Math.random() * wtArray.length - 1)];
+      nameBruh.innerHTML = random;
     }
   };
 });
